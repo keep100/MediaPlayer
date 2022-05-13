4e2e1806d74212f7cbf6abd3ae7d73ef6d2578a3
megumi-tyan <1640963236@qq.com>
2022-05-13 21:27:50 +0800

    优化了一下获取缩略图的函数

--- XMediaManager.cpp
+++ XMediaManager.cpp
@@ -11,6 +11,7 @@ extern "C" {
 #include <QThread>
 #include <QDebug>
 using namespace std;
+
 BriefInfo XMediaManager::getBriefInfo(const char * url){
     BriefInfo briefInfo;
     if (url == 0 || url[0] == '\0')
@@ -31,21 +32,31 @@ BriefInfo XMediaManager::getBriefInfo(const char * url){
 
     if(decode->Open(demux->CopyVPara())){
         AVFrame *pFrame = av_frame_alloc();
+        demux->Seek(0.5);
         //先recv，后send
-        int numFrame = 0;
         while (true)
         {
             pFrame = decode->Recv();
             if (pFrame)
             {
-                numFrame++;
-    //            qDebug() << "receive--------------------------";
-                if(numFrame==1000)break;
+//                qDebug() << "receive--------------------------";
+                QImage temp = getQImageFromFrame(pFrame, demux->CopyVPara());
+                if(!temp.isNull()&&temp.bits()){
+                    int w = temp.width(), h = temp.height();
+                    int rgbValue = (((temp.pixel(0.25*w, 0.25*h)+temp.pixel(0.75*w, 0.75*h)+temp.pixel(0.25*w, 0.75*h)+temp.pixel(0.75*w, 0.25*h))/4));
+                    int means = (qRed(rgbValue)+qGreen(rgbValue)+qBlue(rgbValue))/3;
+//                    qDebug()<<qRed(rgbValue)<<qGreen(rgbValue)<<qBlue(rgbValue)<<means;
+                    if(means>150){
+                        qDebug()<<"有东西的图";
+                        break;
+                    }
+
+                }
+//                break;
             }
-    //        qDebug() << "send--------------------------";
+//            qDebug() << "send--------------------------";
             AVPacket *pkt = demux->ReadVideo();
             qDebug()<<decode->Send(pkt);
-    //        _sleep(1);
         }
         briefInfo.qimage = getQImageFromFrame(pFrame, demux->CopyVPara());
     }
@@ -57,9 +68,8 @@ QImage XMediaManager::getQImageFromFrame(const AVFrame* frame, AVCodecParameters
 
     qDebug() << "frameToImage";
     QImage output(frame->width, frame->height, QImage::Format_RGB32); //构造一个QImage用作输出
-    qDebug()<<frame->width<<", "<<frame->height;
-    qDebug() << "frameToImage2";
-    int outputLineSize[4];                                                       //构造AVFrame到QImage所需要的数据
+//    qDebug()<<frame->width<<", "<<frame->height;
+    int outputLineSize[4];                                            //构造AVFrame到QImage所需要的数据
     av_image_fill_linesizes(outputLineSize, AV_PIX_FMT_RGB32, para->width);
     uint8_t *outputDst[] = {output.bits()};
     //构造一个格式转换上下文
@@ -70,7 +80,7 @@ QImage XMediaManager::getQImageFromFrame(const AVFrame* frame, AVCodecParameters
 
     //把解码得到的损坏的像素数据剔除
     sws_scale(imgConvertContext, frame->data, frame->linesize, 0, para->height, outputDst, outputLineSize);
-    output.save("C:\\Users\\16409\\Desktop\\oneFrame.jpg");
+//    output.save("C:\\Users\\16409\\Desktop\\oneFrame.jpg");
     sws_freeContext(imgConvertContext);
 //    av_frame_free(&frame);
     return output;
