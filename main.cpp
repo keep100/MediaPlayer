07f0209d34e5160ffa8eeeb6443f6b72c5f44cde
LYouC <1786686418@qq.com>
2022-05-23 05:08:48 +0000

    update main.cpp.

--- main.cpp
+++ main.cpp
@@ -19,12 +19,6 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
     QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
     QQmlApplicationEngine engine;
-//    ShowImage *myImage = new ShowImage();
-//    engine.rootContext()->setContextProperty("MyImage", myImage);
-//    engine.addImageProvider(QLatin1String("Imgs"), myImage->m_pImgProvider);
-//    XDemuxThread *thread = new XDemuxThread();
-//    engine.rootContext()->setContextProperty("MyImage", thread->vt->showImage);
-//    engine.addImageProvider(QLatin1String("Imgs"), thread->vt->showImage->m_pImgProvider);
     Controller controller(engine);
     CursorPosProvider mousePosProvider;
     engine.rootContext()->setContextProperty("mousePosition", &mousePosProvider);
@@ -41,29 +35,5 @@ int main(int argc, char *argv[])
 
     qmlRegisterType<MyItem>("MyItem", 1, 0, "MyItem");
     engine.load(url);
-
-    // 打开文件
-//    QString name = "D:/迅雷下载/Young.Sheldon.S01.1080p.AMZN.WEBRip.DDP5.1.x264-NTb[rartv]/Young.Sheldon.S01E01.Pilot.1080p.AMZN.WEB-DL.DDP5.1.H.264-NTb.mkv";
-//    QString name = "C:/Users/13007/Downloads/test.mpeg";
-
-//    XDemuxThread dt;
-//    dt.Start();
-//    if (!dt.Open(name.toLocal8Bit(), nullptr))
-//    {
-//        qDebug() << "dt open fail\n";
-//        return -1;
-//    }
-
-//    QTimer *timer = new QTimer();
-//    QObject::connect(timer, &QTimer::timeout, [&ctrl, &dt](){
-//        std::shared_ptr<YUVData> data = dt.resendYUV();
-//        if (data != nullptr) {
-//            ctrl.onUpdate(data, 0);
-//            qDebug() << "ll\n";
-//        }
-
-//    });
-//    timer->start(1000 / 24.0);
-
     return app.exec();
 }
