c247ac479144e07829f7298708590a475900f658
蔡陆培 <1300791830@qq.com>
2022-05-24 10:09:34 +0000

    update XMediaManager.h.

--- XMediaManager.h
+++ XMediaManager.h
@@ -18,17 +18,17 @@ struct BriefInfo{
     QString mediaType = "";     //媒体类型 （"video", "audio", ""）
     QString codecId = "";       //编码格式
     int totalMs = 0;            //总时长（ms）
-    int bitRate = 0;            //比特率（bps）
     int channels = 0;           //声道数
     QImage img;                 //歌曲的封面或视频的缩略图
-    //视频特有信息
+    int aBitRate = 0;            //音频比特率（bps）
+    //视频信息
+    int vBitRate = 0;            //视频比特率（bps）
     int width = 0;
     int height = 0;
-    //歌曲特有信息
+    //歌曲或音频的信息
     QString album = "";         //唱片集
     QString artist = "";        //参与创作的艺人
     QString title = "";         //标题
-
 };
 
 
